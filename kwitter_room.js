1a27b0c3c96a8e868991c38bdfda11bd8e1ada4e
Aarya's Rep <116066096+Aaryadapro@users.noreply.github.com>
2023-05-24 17:46:30 +1200

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDpnd8f178Jt3q_N3hJ9r7R_25wDTiOicg",
+    authDomain: "letschat-ce688.firebaseapp.com",
+    projectId: "letschat-ce688",
+    storageBucket: "letschat-ce688.appspot.com",
+    messagingSenderId: "660894874853",
+    appId: "1:660894874853:web:ee49b1fecaa96cef52e80c"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
